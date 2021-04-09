a8b34d90ce684fcf01e04b59ad90e0df5da49a9c
João Silva <paradoxsmurf@outlook.com>
2021-04-09 03:21:25 +0100

    almost done with quiz

--- js/masterplan.js
+++ js/masterplan.js
@@ -1,7 +1,11 @@
 $(function(){
     var loading = $('#loadbar').hide();
     var i = 0;
+    var score = 0;
     document.querySelector('#questionText').innerHTML = questions[i];
+    document.querySelector('#answerOne').innerHTML = answers[i][0];
+    document.querySelector('#answerTwo').innerHTML = answers[i][1];
+    document.querySelector('#answerThree').innerHTML = answers[i][2];
     $(document)
     .ajaxStart(function () {
         loading.show();
@@ -11,38 +15,38 @@ $(function(){
 
         $("label.btn").on('click',function () {
 
-            if(i >= 6){
-                document.querySelector('#questionText').innerHTML = "Quiz Done";
+            if(i >= 3){
+                document.querySelector('#questionText').innerHTML = "Quiz Done<br>Total Score: " + score;
                 $('#quiz').hide();
             }else{
-                var choice = $(this).find('input:radio').val();
+                var choice = 0;
+                console.log($(this));
                 $('#loadbar').show();
                 $('#question').hide();
                 $('#quiz').hide();
                 i++;
                 document.querySelector('#questionText').innerHTML = questions[i];
-                console.log(i);
+                document.querySelector('#answerOne').innerHTML = answers[i][0];
+                document.querySelector('#answerTwo').innerHTML = answers[i][1];
+                document.querySelector('#answerThree').innerHTML = answers[i][2];
 
                 setTimeout(function(){
-                    $('#answer').html($(this).checking(choice));
                     $('#question').show();
                     $('#quiz').show();
                     $('#loadbar').hide();
-                }, 1500);
+                }, 1000);
             }
         });
-
-    $ans = 3;
-
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
-        else 
-            return 'CORRECT';
-    }; 
 });
 
-var questions = ["If you could genetically cross two animals to get the best of both, which would you choose?", "",
-    "Why do you want to take over the world?", "",
-    "If you saw an orange crossing the road, what would you do?", "",
+var questions = ["If you could genetically cross two animals to get the best of both, which would you choose?",
+    "Why do you want to take over the world?",
+    "If you saw an orange crossing the road, what would you do?",
     "If you had God powers for 30 minutes, you would"]
+
+var answers = {
+    0: ["A mouse and a dinosaur", "A penguin and a cat", "A lion and an eagle"],
+    1: ["I got a bit bored watching Netflix", "I am the supreme being, and everyone shall bow to me", "The world is a sad place and needs my guidance"],
+    2: ["I would take her hand and help her accross the road", "Grab it and make an orange juice", "Take a cold shower to wake up"],
+    3: ["Make all pokemon real", "Predict the future and play at the casino", "Transfer all the gold and currency to my personal bank"]
+}
